ded28977c53e6102cf79345cbac2880c3c0c5ef5
sroz <seanmcdowall@protonmail.com>
2020-08-11 12:45:39 +0100

    Finished

---
+++ FooBarQix/NumberRulesHelper.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Xml.Schema;
+
+namespace FooBarQix
+{
+    public enum AppendTypes
+    {
+        Foo, Bar, Qix
+    }
+    public class NumberRulesHelper
+    {
+
+        public string displayString(long number)
+        {
+            //amended string to be displayed - initalise to empty
+            string returnString = string.Empty;
+            bool isAnyDivisable = false;
+
+            //business rule added, bool flags presence of 0 in provided number
+            bool containsZero = false;
+
+            int zeroCount = 0;
+
+            if (number.ToString().Contains("0"))
+            {
+                containsZero = true;
+            }
+
+            //block to filter number to check if divisible by 3,5,7 to append string output
+
+            if (containsZero)
+            {
+                //createCountOfZeros
+                zeroCount = number.ToString().Count(z => (z == '0'));
+            }
+
+
+            if (IsDivisable(number, 3))
+            {
+                returnString += AppendTypes.Foo;
+                isAnyDivisable = true;
+            }
+            if (IsDivisable(number, 5))
+            {
+                returnString += AppendTypes.Bar;
+                isAnyDivisable = true;
+            }
+            if (IsDivisable(number, 7))
+            {
+                returnString += AppendTypes.Qix;
+                isAnyDivisable = true;
+            }
+
+            //trace 0
+            if (returnString.Contains("0"))
+            {
+                returnString.Replace("0", "*");
+            }
+
+            if (!isAnyDivisable)
+            {
+                //as return string empty we can use the number as a string as no manipulation occured as of yet.
+                var replacedDisplay = number.ToString().Replace('0', '*');
+                //replacedDisplay = DigitIncludedInString(replacedDisplay);
+
+                //go and find if the number provided contains a char 3,5,7
+                var replacedDisplayNumber = ContainsNumber(number.ToString());
+                if (replacedDisplayNumber != string.Empty)
+                {
+                    return replacedDisplayNumber;
+                }
+
+                return replacedDisplay;
+
+
+            }
+
+            //append the zero trace to the string to be displayed 
+            if (zeroCount > 0)
+            {
+                for (int i = 0; i < zeroCount; i++)
+                {
+                    returnString += "*";
+                }
+            }
+
+            if (isAnyDivisable)
+            {
+                //append to the end of the string if any 3,5,7 are in the input number
+                returnString += ContainsNumber(number.ToString());
+            }
+            return returnString;
+        }
+
+
+        //method which calculates if the number provided includes a 3,5,7. append string if so.
+        public string ContainsNumber(string number)
+        {
+            char[] n = number.ToString().ToCharArray();
+            string returnStringtoDisplay = string.Empty;
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == '3')
+                {
+                    returnStringtoDisplay += AppendTypes.Foo;
+                }
+                else if (n[i] == '5')
+                {
+                    returnStringtoDisplay += AppendTypes.Bar;
+                }
+                else if (n[i] == '7')
+                {
+                    returnStringtoDisplay += AppendTypes.Qix;
+                }
+            }
+
+            return returnStringtoDisplay;
+        }
+
+        public bool IsDivisable(long number, int divider)
+        {
+            try
+            {
+                return number % divider == 0;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+
+    }
+
+}
---
+++ FooBarQix/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FooBarQix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple menu
+            bool KeepGoing = true;
+            while (KeepGoing)
+            {
+                KeepGoing = MainOperation();
+            }
+
+        }
+
+        private static bool MainOperation()
+        {
+            try
+            {
+                Console.WriteLine("Enter Number");
+                var numberInputted = Convert.ToInt64(Console.ReadLine());
+                if (numberInputted == 0 || numberInputted < 0)
+                {
+                    Console.WriteLine("Enter a value larger than 0");
+                    return true;
+                }
+                else
+                {
+                    var displayString = Compute(numberInputted);
+                    Console.WriteLine(displayString);
+                    Console.WriteLine("Retry? Y/N");
+                    var carryOn = UserChoice();
+                    return carryOn;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Program Failure : {ex.Message}");
+                Console.WriteLine("Continue and try again? Y/N");
+                var carryOn = UserChoice();
+                return carryOn;
+            }
+        }
+
+        static string Compute(long inputNumber)
+        {
+            var numberHelper = new NumberRulesHelper();
+            var returnedString = numberHelper.displayString(inputNumber);
+            return returnedString;
+        }
+
+        //user message block
+        static bool UserChoice()
+        {
+            // Console.WriteLine("Retry? Y/N");
+            var carryOnWithProgram = Console.ReadKey();
+            if (carryOnWithProgram.Key == ConsoleKey.Y || carryOnWithProgram.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
